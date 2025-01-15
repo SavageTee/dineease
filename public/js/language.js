f6de43382a6bf79b90481fffa51a045f1b204737
Talal Elshazly <talalelshazly@gmail.com>
2025-01-15 23:13:21 +0200

    2255

--- public/js/language.js
+++ public/js/language.js
@@ -5,7 +5,26 @@ function redirect(chosenLanguage){
     console.log(newUrl)
     window.location.href = newUrl;
 }
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+if (isDarkMode) {
+    $('html').addClass('tw-dark');  // Apply dark mode
+    $('#sun-icon').show()
+    $('#moon-icon').hide()
+} else {
+    $('html').removeClass('tw-dark');  // Apply light mode
+    $('#sun-icon').hide()
+    $('#moon-icon').show()
+}
+
+
 $(document).ready(function() {
+    $('#theme-toggle').click(function() {
+        $('html').toggleClass('tw-dark');
+        $('#sun-icon').toggle()
+        $('#moon-icon').toggle()
+        const darkModeEnabled = $('html').hasClass('tw-dark');
+        localStorage.setItem('darkMode', darkModeEnabled);
+    });  
     $('#en').on('click',()=>{redirect('en');})
     $('#ar').on('click',()=>{redirect('ar');})
     $('#de').on('click',()=>{redirect('de');})
